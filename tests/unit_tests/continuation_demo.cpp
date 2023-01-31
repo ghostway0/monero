f92344697c183f7ca786b1f277c2a40d78471552
koe <ukoe@protonmail.com>
2023-01-31 17:12:50 -0600

    continuation demo: use task graph monitor to manage cancellation, the result, and tracking progress

--- tests/unit_tests/continuation_demo.cpp
+++ tests/unit_tests/continuation_demo.cpp
@@ -35,6 +35,7 @@
 #include <iostream>
 #include <memory>
 #include <queue>
+#include <stdexcept>
 
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
@@ -113,6 +114,103 @@ static void mul_int(const int x, int &i_inout)
 }
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
+template <typename T>
+bool future_is_ready(const std::future<T> &future)
+{
+    if (!future.valid())
+        return false;
+    if (future.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+        return false;
+    return true;
+}
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+template <typename T>
+bool future_is_ready(const std::shared_future<T> &future)
+{
+    if (!future.valid())
+        return false;
+    if (future.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+        return false;
+    return true;
+}
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+template <typename R>
+expect<R> unwrap_future(std::future<R> &future)
+{
+    if (!future_is_ready(future)) { return std::error_code{};       }
+    try                           { return std::move(future.get()); }
+    catch (std::error_code e)     { return e;                       }
+    catch (...)                   { return std::error_code{};       }
+}
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+template <typename T>
+struct Task final
+{
+    unsigned char id;
+    T task;
+};
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+template <typename T>
+Task<T> make_task(const unsigned char id, T &&task)
+{
+    return Task<T>{.id = id, .task = std::forward<T>(task)};
+}
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+template <typename>
+class TaskGraphMonitorBuilder;
+
+template <typename R>
+class TaskGraphMonitor final
+{
+    friend class TaskGraphMonitorBuilder<R>;
+
+public:
+    bool is_canceled() const { return future_is_ready(m_cancellation_flag); }
+    bool has_result()  const { return future_is_ready(m_final_result);      }
+
+    void cancel() { if (!this->is_canceled()) m_cancellation_handle.set_value(); }
+    expect<R> expect_result() { return unwrap_future(m_final_result); }
+
+protected:
+    std::promise<void> m_cancellation_handle;
+    std::shared_future<void> m_cancellation_flag;
+    std::future<R> m_final_result;
+};
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+template <typename R>
+class TaskGraphMonitorBuilder final
+{
+public:
+    TaskGraphMonitorBuilder(std::future<R> graph_future_result)
+    {
+        m_monitor.m_final_result = std::move(graph_future_result);
+        m_monitor.m_cancellation_flag = m_monitor.m_cancellation_handle.get_future().share();
+    }
+
+    std::shared_future<void> add_task(const unsigned char task_id, std::future<void> task_completion_flag)
+    {
+        return m_monitor.m_cancellation_flag;
+    }
+
+    TaskGraphMonitor<R> get_monitor()
+    {
+        if(!m_monitor.m_cancellation_flag.valid())
+            throw std::runtime_error{"task graph monitor builder: already extracted monitor."};
+
+        return std::move(m_monitor);
+    }
+
+private:
+    TaskGraphMonitor<R> m_monitor;
+};
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
 template <typename I, typename T, typename R>
 static auto initialize_future_task(I &&initial_value, T &&task, std::promise<R> promise)
 {
@@ -129,34 +227,72 @@ static auto initialize_future_task(I &&initial_value, T &&task, std::promise<R> 
 //-------------------------------------------------------------------------------------------------------------------
 // end case: set the promise from the task result
 //-------------------------------------------------------------------------------------------------------------------
-template <typename R, typename S, typename A>
-static auto build_task_tree(S, A &&this_task)
+template <typename R, typename S, typename T>
+static auto build_task_graph(TaskGraphMonitorBuilder<R> &graph_monitor_builder_inout, S, Task<T> &&this_task)
 {
+    std::promise<void> completion_handle{};
+    std::shared_future<void> cancellation_flag{
+            graph_monitor_builder_inout.add_task(this_task.id, completion_handle.get_future())
+        };
+
     return
         [
-            l_task = std::forward<A>(this_task)
+            l_completion_handle = std::move(completion_handle),
+            l_task              = std::forward<Task<T>>(this_task).task,
+            l_cancellation_flag = std::move(cancellation_flag)
         ] (auto&& this_task_val, std::promise<R> promise) mutable -> void
         {
-            try { promise.set_value(l_task(std::move(this_task_val))); }
+            // check for cancellation
+            if (future_is_ready(l_cancellation_flag))
+                return;
+
+            // execute the task
+            try
+            {
+                promise.set_value(l_task(std::move(this_task_val)));
+                l_completion_handle.set_value();
+            }
             catch (...)
             {
-                try { promise.set_exception(std::current_exception()); } catch (...) { /*can't do anything*/ }
+                try
+                {
+                    promise.set_exception(std::current_exception());
+                    l_completion_handle.set_exception(std::current_exception());
+                } catch (...) { /*can't do anything*/ }
             }
         };
 }
 //-------------------------------------------------------------------------------------------------------------------
-// fold into task 'a' its continuation 'the rest of the task tree'
+// fold into task 'a' its continuation 'the rest of the task graph'
 //-------------------------------------------------------------------------------------------------------------------
-template <typename R, typename S, typename A, typename... Ts>
-static auto build_task_tree(S scheduler, A &&this_task, Ts &&...continuation_tasks)
+template <typename R, typename S, typename T, typename... Ts>
+static auto build_task_graph(TaskGraphMonitorBuilder<R> &graph_monitor_builder_inout,
+    S scheduler,
+    Task<T> &&this_task,
+    Task<Ts> &&...continuation_tasks)
 {
+    std::promise<void> completion_handle{};
+    std::shared_future<void> cancellation_flag{
+            graph_monitor_builder_inout.add_task(this_task.id, completion_handle.get_future())
+        };
+
     return
         [
-            l_scheduler = scheduler,
-            l_this_task = std::forward<A>(this_task),
-            l_next_task = build_task_tree<R>(scheduler, std::forward<Ts>(continuation_tasks)...)
+            l_completion_handle = std::move(completion_handle),
+            l_scheduler         = scheduler,
+            l_this_task         = std::forward<Task<T>>(this_task).task,
+            l_next_task         = build_task_graph<R>(
+                                        graph_monitor_builder_inout,
+                                        scheduler,
+                                        std::forward<Task<Ts>>(continuation_tasks)...
+                                    ),
+            l_cancellation_flag = std::move(cancellation_flag)
         ] (auto&& this_task_val, std::promise<R> promise) mutable -> void
         {
+            // check for cancellation
+            if (future_is_ready(l_cancellation_flag))
+                return;
+
             // this task's job
             auto this_task_result =
                 [&]() -> expect<decltype(l_this_task(std::declval<decltype(this_task_val)>()))>
@@ -164,13 +300,21 @@ static auto build_task_tree(S scheduler, A &&this_task, Ts &&...continuation_tas
                     try { return l_this_task(std::forward<decltype(this_task_val)>(this_task_val)); }
                     catch (...)
                     {
-                        try { promise.set_exception(std::current_exception()); } catch (...) { /*can't do anything*/ }
+                        try
+                        {
+                            promise.set_exception(std::current_exception());
+                            l_completion_handle.set_exception(std::current_exception());
+                        } catch (...) { /*can't do anything*/ }
                         return std::error_code{};
                     }
                 }();
 
             // give up if this task failed
             if (!this_task_result)
+                return;
+
+            // check for cancellation again (can discard the task result if cancelled)
+            if (future_is_ready(l_cancellation_flag))
                 return;
 
             // pass the result of this task to the continuation
@@ -182,39 +326,33 @@ static auto build_task_tree(S scheduler, A &&this_task, Ts &&...continuation_tas
 
             // submit the continuation task to the scheduler
             l_scheduler(std::move(continuation));
+
+            // mark success
+            l_completion_handle.set_value();
         };
 }
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 template <typename R, typename S, typename I, typename... Ts>
-static std::future<R> schedule_task_tree(S scheduler, I &&initial_value, Ts &&...tasks)
+static TaskGraphMonitor<R> schedule_task_graph(S scheduler, I &&initial_value, Task<Ts> &&...tasks)
 {
     // prepare result channel
     std::promise<R> result_promise;
     std::future<R> result_future{result_promise.get_future()};
 
-    // build task tree
-    auto task_tree_head = initialize_future_task(
+    // build task graph
+    TaskGraphMonitorBuilder<R> monitor_builder{std::move(result_future)};
+    auto task_graph_head = initialize_future_task(
             std::forward<I>(initial_value),
-            build_task_tree<R>(scheduler, std::forward<Ts>(tasks)...),
+            build_task_graph<R>(monitor_builder, scheduler, std::forward<Task<Ts>>(tasks)...),
             std::move(result_promise)
         );
 
-    // schedule task tree
-    scheduler(std::move(task_tree_head));
+    // schedule task graph
+    scheduler(std::move(task_graph_head));
 
     // return future
-    return result_future;
-}
-//-------------------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------------
-template <typename R>
-expect<R> unwrap_future(std::future<R> future)
-{
-    if (!future.valid())      { return std::error_code{};       }
-    try                       { return std::move(future.get()); }
-    catch (std::error_code e) { return e;                       }
-    catch (...)               { return std::error_code{};       }
+    return monitor_builder.get_monitor();
 }
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
@@ -234,26 +372,29 @@ static auto basic_continuation_demo_test(S scheduler)
     //   task 4a-2: mul10
     // task 4 JOIN: add together each branch
     // task 5 print
-    auto job1 =
-        [](int val) -> int
-        {
-            print_int(val);
-            return val;
-        };
-    auto job2 =
-        [
-            addor = std::move(add_five)
-        ] (int val) -> int
-        {
-            add_int(addor, val);
-            return val;
-        };
-    auto job3 =
-        [](int val) -> int
-        {
-            print_int(val);
-            return val;
-        };/*
+    auto job1 = make_task(1,
+            [](int val) -> int
+            {
+                print_int(val);
+                return val;
+            }
+        );
+    auto job2 = make_task(2,
+            [
+                addor = std::move(add_five)
+            ] (int val) -> int
+            {
+                add_int(addor, val);
+                return val;
+            }
+        );
+    auto job3 = make_task(3,
+            [](int val) -> int
+            {
+                print_int(val);
+                return val;
+            }
+        );/*
     auto job4 =
         [
             multiplier = std::move(mul_three)
@@ -299,14 +440,14 @@ static auto basic_continuation_demo_test(S scheduler)
             return val;
         };*/
 
-    // build task tree and schedule it
-    return schedule_task_tree<int>(
+    // build task graph and schedule it
+    return schedule_task_graph<int>(
             scheduler,
             std::move(initial_val),
             std::move(job1),
             std::move(job2),
             std::move(job3)/*,
-            task_tree_openclose(
+            task_graph_openclose(
                 std::move(job4_split),
                 std::make_tuple(std::move(job4a_1), std::move(job4a_2)),
                 std::make_tuple(std::move(job4b_1)),
@@ -320,14 +461,22 @@ static auto basic_continuation_demo_test(S scheduler)
     //   test when all joiners are done)
     // - if an exception cancels a branch, that cancellation needs to immediately propagate to all dependents
     //   - a split should only be canceled if A) at least one parent is canceled, or B) if all children are canceled
-    // - want to be able to manually cancel a task? maybe collect cancellation tokens while building a tree...
+    // - want to be able to manually cancel a task? maybe collect cancellation tokens while building a graph...
+
+    // todo
+    // - is_canceled() callback for tasks that can cancel themselves
+    // - make detached task graph by moving the task graph monitor into the last task's lambda capture
+    //   - detached graphs should have void return type (last task returns nothing)
+    //   - detached graphs can be built sideways within a large graph construction, using a fresh monitor builder
+    //   - detached graphs are not cancellable; if a cancellable process is desired, don't use a detached graph, just
+    //     use a normal graph and keep track of the graph monitor (which will auto-cancel the graph when destroyed)
 }
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 TEST(continuation_demo, basic_autorun)
 {
     // run the test with a scheduler that immediately invokes tasks
-    auto task_tree_future = basic_continuation_demo_test(
+    TaskGraphMonitor<int> task_graph_monitor = basic_continuation_demo_test(
             [](auto&& task)
             {
                 task();
@@ -335,8 +484,8 @@ TEST(continuation_demo, basic_autorun)
         );
 
     // extract final result
-    EXPECT_TRUE(task_tree_future.valid());
-    const expect<int> final_result{unwrap_future<int>(std::move(task_tree_future))};
+    EXPECT_TRUE(task_graph_monitor.has_result());
+    const expect<int> final_result{task_graph_monitor.expect_result()};
     EXPECT_TRUE(final_result);
     EXPECT_NO_THROW(final_result.value());
     std::cerr << "final result: " << final_result.value() << '\n';
@@ -345,7 +494,7 @@ TEST(continuation_demo, basic_autorun)
 TEST(continuation_demo, basic_threadpool)
 {
     // run the test with a scheduler that sends tasks into the demo threadpool
-    auto task_tree_future = basic_continuation_demo_test(
+    TaskGraphMonitor<int> task_graph_monitor = basic_continuation_demo_test(
             [](auto&& task)
             {
                 add_task_to_demo_threadpool(std::forward<decltype(task)>(task));
@@ -361,8 +510,8 @@ TEST(continuation_demo, basic_threadpool)
     }
 
     // extract final result
-    EXPECT_TRUE(task_tree_future.valid());
-    const expect<int> final_result{unwrap_future<int>(std::move(task_tree_future))};
+    EXPECT_TRUE(task_graph_monitor.has_result());
+    const expect<int> final_result{task_graph_monitor.expect_result()};
     EXPECT_TRUE(final_result);
     EXPECT_NO_THROW(final_result.value());
     std::cerr << "final result: " << final_result.value() << '\n';
