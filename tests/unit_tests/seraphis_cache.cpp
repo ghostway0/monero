b989b65aa2fd18b4919c35c92fe2ac9967ce809e
ghostway <ghostway@tuta.io>
2023-02-25 23:46:58 +0200

    first day of work

--- tests/unit_tests/seraphis_cache.cpp
+++ tests/unit_tests/seraphis_cache.cpp
@@ -1,30 +1,30 @@
 #include "seraphis_cache.h"
+#include "crypto/crypto.h"
+#include "crypto/hash.h"
 
+#include "gtest/gtest.h"
 #include <boost/range/adaptor/reversed.hpp>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <ctime>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <map>
 
-bool HalfPruning::should_prune(const std::deque<uint64_t> &window, uint64_t chain_top)
+uint64_t Cache::get_nearest_block_height_clampdown(const uint64_t test_height)
 {
-    if (chain_top - *window.end() <= m_num_unprunable)
-        return false;
-
-    uint64_t width = *window.end() - *window.begin();
-    return window.size() * m_max_separation > width;
-}
-
-uint64_t Cache::get_nearest_block_height_clampdown(uint64_t test_height)
-{
-    if (test_height < m_refresh_height)
-        return (uint64_t)(-1);
+    assert(test_height > m_refresh_height);
 
     auto checkpoint = m_checkpoints.upper_bound(test_height);
-    if (checkpoint == m_checkpoints.end())
+    if (checkpoint == m_checkpoints.end() || checkpoint == m_checkpoints.begin())
         return (uint64_t)(-1);
 
     return (*--checkpoint).first;
 }
 
-void Cache::insert_new_block_ids(uint64_t first_block_height, std::vector<crypto::hash> block_ids)
+void Cache::insert_new_block_ids(const uint64_t first_block_height, const std::vector<crypto::hash> &block_ids)
 {
     auto erase_begin = m_checkpoints.lower_bound(first_block_height);
     if (erase_begin != m_checkpoints.end())
@@ -35,40 +35,83 @@ void Cache::insert_new_block_ids(uint64_t first_block_height, std::vector<crypto
     for (size_t i = 0; i < block_ids.size(); i++)
     {
         uint64_t current_height = first_block_height + i;
-        m_checkpoints.insert({current_height, block_ids[i]});
+        m_checkpoints[current_height] = block_ids[i];
     }
 
-    clean_prunable_checkpoints();
+    if (!m_checkpoints.empty())
+        this->clean_prunable_checkpoints();
 }
 
 void Cache::clean_prunable_checkpoints()
 {
-    uint64_t latest_height = m_checkpoints.end()->first;
+    assert(!m_checkpoints.empty());
+
+    uint64_t latest_height = m_checkpoints.rbegin()->first;
     std::deque<uint64_t> window{};
-    window.resize(m_window_size / 2 - 2, m_refresh_height - m_max_separation);
 
-    for (const auto &checkpoint : boost::adaptors::reverse(m_checkpoints))
+    auto checkpoint = m_checkpoints.lower_bound(latest_height - m_num_unprunable);
+    if (checkpoint == m_checkpoints.end() || checkpoint == m_checkpoints.begin())
+        return;
+    --checkpoint;
+
+    while (checkpoint != m_checkpoints.begin())
     {
-        window.push_back(checkpoint.first);
+        window.push_back(checkpoint->first);
 
-        if (window.size() >= m_window_size)
+        if (should_prune(window, latest_height))
         {
-            if (m_strategy.should_prune(window, latest_height))
-            {
-                uint64_t middle = window.at(window.size() / 2);
-                m_checkpoints.erase(middle);
-            }
-
-            m_checkpoints.erase(*window.begin());
+            auto middle = m_checkpoints.lower_bound(window[window.size() / 2]);
+            checkpoint = --m_checkpoints.erase(middle);
+            window.erase(window.begin() + window.size() / 2);
+        } else {
+            --checkpoint;
         }
+    }
+
+    checkpoint = m_checkpoints.begin();
+    while (checkpoint != m_checkpoints.end() && stored_checkpoints() > m_max_cached_checkpoints)
+    {
+        // TODO: I don't think max_cached_checkpoints should overrule num_unprunable.
+        checkpoint = m_checkpoints.erase(checkpoint);
     }
 }
 
-TEST(seraphis_cache, main)
+bool Cache::should_prune(const std::deque<uint64_t> &window, const uint64_t chain_top)
 {
-    HalfPruning h(10, 100000);
-    Cache cache(100, 0, 10, h);
+    assert(window.size() > 0 && chain_top - window.front() > m_num_unprunable);
 
-    cache.insert_new_block_ids(10, {});
-    // something something
+    uint64_t delta = chain_top - window.front(); // how to incorporate this? sqrt(delta) grows too fast.
+    uint64_t width = window.front() - window.back();
+
+    return window.size() * m_max_separation > width; // doesn't seem like max separation anymore.
+}
+
+std::vector<crypto::hash> create_dummy_blocks(uint64_t size)
+{
+    std::vector<crypto::hash> dummy(size, crypto::null_hash);
+    return dummy;
+}
+
+TEST(seraphis_cache, exceed_max_checkpoints)
+{
+    uint64_t max_checkpoints = 1;
+
+    Cache cache(max_checkpoints, 0, 0, 10); // max_separation=0, make sure to change this if `should_prune` changes
+    auto dummy = create_dummy_blocks(20);
+    cache.insert_new_block_ids(0, dummy);
+
+    ASSERT_TRUE(cache.stored_checkpoints() == 1);
+}
+
+TEST(seraphis_cache, check_erases)
+{
+    // erasing and decrementing, dangerous stuff.
+    Cache cache(30, 0, 100, 10);
+    auto dummy = create_dummy_blocks(20);
+    cache.insert_new_block_ids(0, dummy);
+}
+
+TEST(seraphis_cache, greater_refresh)
+{
+    // refresh height > latest_height - num_unprunable?
 }
